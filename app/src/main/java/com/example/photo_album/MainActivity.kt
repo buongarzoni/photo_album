17fe80466a65f211b6c2a7fcfce562f5f2a72f06
cbuongarzoni <cbuongarzoni@est.frba.utn.edu.ar>
2022-02-12 20:01:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/photo_album/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.photo_album
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.photo_album.ui.theme.Photo_albumTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Photo_albumTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Photo_albumTheme {
+        Greeting("Android")
+    }
+}
