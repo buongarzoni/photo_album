a809f11b8a2c4d909717501182ba8882b45e7e14
cbuongarzoni <cbuongarzoni@est.frba.utn.edu.ar>
2022-02-15 13:33:25 -0300

    :lipstick: DetailScreen with gradient with text over image when clicked

--- app/src/main/java/com/example/photo_album/photo_detail/infrastructure/PhotoDetailScreen.kt
+++ app/src/main/java/com/example/photo_album/photo_detail/infrastructure/PhotoDetailScreen.kt
@@ -1,20 +1,38 @@
 package com.example.photo_album.photo_detail.infrastructure
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import com.example.photo_album.R
 import com.example.photo_album.album_list.application.AlbumsViewModel
 import com.example.photo_album.photo_detail.domain.PhotoModel
 import com.example.photo_album.utils.infrastructure.MainTopAppBar
 
+@ExperimentalCoilApi
 @Composable
 fun PhotoDetailScreen(albumName: String, photoName: String, viewModel: AlbumsViewModel) {
     Scaffold(
@@ -23,6 +41,7 @@ fun PhotoDetailScreen(albumName: String, photoName: String, viewModel: AlbumsVie
     )
 }
 
+@ExperimentalCoilApi
 @Composable
 private fun ScreenContent(albumName: String, photoName: String, viewModel: AlbumsViewModel){
     val photo = viewModel.state.value
@@ -35,17 +54,111 @@ private fun ScreenContent(albumName: String, photoName: String, viewModel: Album
 
     DisplayPhotoDetails(photo = photo)
 }
+
+@ExperimentalCoilApi
 @Composable
 private fun DisplayPhotoDetails(photo: PhotoModel) {
+    val mustShowDetails = remember{ mutableStateOf(false) }
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(MaterialTheme.colors.background),
+        .background(MaterialTheme.colors.background)
+    ) {
+        AddImage(photoUrl = photo.photo_url, name = photo.name, mustShowDetails = mustShowDetails)
+        ShowDetails(photo = photo, mustShowDetails = mustShowDetails)
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+private fun AddImage(photoUrl: String, name: String, mustShowDetails: MutableState<Boolean>) {
+    val painter = rememberImagePainter(data = photoUrl)
+
+    if (painter.state is ImagePainter.State.Loading) {
+        CircularProgressIndicator()
+    }
+
+    Image(painter = painter, contentDescription = name, modifier = Modifier
+        .fillMaxSize()
+        .clickable { mustShowDetails.value = !mustShowDetails.value },
+    alignment = Alignment.Center)
+}
+
+@Composable
+private fun ShowDetails(photo: PhotoModel, mustShowDetails: MutableState<Boolean>) {
+    val screenHeight = LocalConfiguration.current.screenHeightDp
+    AnimatedVisibility (
+        visible = mustShowDetails.value,
+        enter = enterAnimatedTransition(screenHeight = screenHeight),
+        exit = exitAnimatedTransition(screenHeight = screenHeight)
+    ) {
+        AddGradient()
+        AddDetails(photo = photo)
+    }
+}
+
+@Composable
+private fun AddGradient() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Brush.verticalGradient(colors = listOf(Color.Transparent, Color.Black)))
+    )
+}
+
+@Composable
+private fun AddDetails(photo: PhotoModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
         contentAlignment = Alignment.Center
     ) {
-        Column{
-            Text(text = "Descripción : ${photo.description}")
-            Text(text = "URL : ${photo.photo_url}")
-            Text(text = "Creado : ${photo.dateCreated}")
+        Column {
+
+            Text(
+                text = stringResource(id = R.string.photo_detail_screen_description),
+                style = styleForTextOverGradient()
+            )
+            Text(
+                text = photo.description,
+                style = styleForTextOverGradient()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Text(
+                    text = "${stringResource(id = R.string.photo_detail_screen_date_created)} : ${photo.dateCreated}",
+                    style = styleForTextOverGradient()
+                )
+            }
         }
+
     }
+}
+
+@Composable
+private fun enterAnimatedTransition(screenHeight: Int): EnterTransition {
+    return slideIn(
+        animationSpec = tween(500),
+        initialOffset = { IntOffset(0, screenHeight*2) }
+    )
+}
+
+@Composable
+private fun exitAnimatedTransition(screenHeight: Int): ExitTransition {
+    return slideOut(
+        animationSpec = tween(500),
+        targetOffset = { IntOffset(0, screenHeight*2) }
+    )
+}
+
+@Composable
+private fun styleForTextOverGradient(): TextStyle{
+    return TextStyle(
+        color = Color.White,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal
+    )
 }
